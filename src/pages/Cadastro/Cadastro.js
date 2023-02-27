d517b6cbe04c6508845aea674ac8cd87055f751d
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2023-02-26 21:11:04 -0300

    reajustado

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,7 +1,7 @@
 import "./Cadastro.css";
 
 import Navbar from "../../componentes/Navbar/Navbar1";
-import aureola1 from "../../imgs/aurela.svg";
+// import aureola1 from "../../imgs/aurela.svg";
 
 import { Container, Row, Col, Form, Input } from "react-bootstrap";
 import { Auth } from "../../componentes/auth/Auth";
@@ -9,68 +9,59 @@ import { auth } from "../../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { useState } from "react";
 
-
-
-
-
 function Cadastro() {
-
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
   onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser)
-  })
+    setUser(currentUser);
+  });
 
   return (
     <>
       <Navbar></Navbar>
-
       <Container>
-        <Row >  
-        
-        <div className="containerr">
+        <Row>
+          <div className="containerr">
             <div className="innerCont">
-                <h1 className="titulo">Cadastro</h1>
-                <h2>{user.email}</h2>
-                    <div className="separador">
-                        <div className="left">
-                            <Auth
-                            placeholderemail='piroca'
-                            placeholderuser='piroca'
-                            placeholderpass='piroca'
-                            placeholderpass2='piroca'
-                            />
-                        </div>
-                        <div className="right">
-                            <button type="button" className="login-with-google-btn" >
-                            Registrar-se com Google
-                            </button>
+              <h1 className="titulo">Cadastro</h1>
+              <h2>{user.email}</h2>
+              <div className="separador">
+                <div className="left">
+                  <Auth
+                    placeholderemail="piroca"
+                    placeholderuser="piroca"
+                    placeholderpass="piroca"
+                    placeholderpass2="piroca"
+                  />
+                </div>
+                <div className="right">
+                  <button type="button" className="login-with-google-btn">
+                    Registrar-se com Google
+                  </button>
 
-                            <button type="button" className="login-with-google-btn" id="face" >
-                            Registrar-se com Facebook
-                            </button>
+                  <button
+                    type="button"
+                    className="login-with-google-btn"
+                    id="face"
+                  >
+                    Registrar-se com Facebook
+                  </button>
 
-                            {/* <button type="button" className="login-with-google-btn" disabled>
+                  {/* <button type="button" className="login-with-google-btn" disabled>
                             Sign in with Google
                             </button> */}
-                        </div>
-                    </div>
+                </div>
+              </div>
             </div>
-
-        </div>
-        
+          </div>
         </Row>
       </Container>
-
-
-
-
-    //   <Container fluid>
-    //     <Navbar></Navbar>
-    //   </Container>
-
+      //{" "}
+      <Container fluid>
+        // <Navbar></Navbar>
+        //{" "}
+      </Container>
     </>
-
   );
 }
 
